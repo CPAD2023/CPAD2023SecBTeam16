247b88bf8dc652003984812fe650268ce2191eaf
Hiral Visaria <hiral.visaria@sap.com>
2023-11-03 15:13:43 +0530

    add places screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -10,6 +10,8 @@ import SavedScreen from "./screens/SavedScreen";
 import BookingScreen from "./screens/BookingScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import SearchScreen from "./screens/SearchScreen";
+import PlacesScreen from './screens/PlacesScreen';
 
 const StackNavigator = () => {
     const Tab = createBottomTabNavigator();
@@ -85,6 +87,8 @@ const StackNavigator = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Main" component={BottomTabs} options={{ headerShown: false }} />
+                <Stack.Screen name="Search" component={SearchScreen} options={{ headerShown:false }} />
+                <Stack.Screen name="Places" component={PlacesScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
